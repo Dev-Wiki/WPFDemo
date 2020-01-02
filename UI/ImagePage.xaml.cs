9224cd97932c31f5b1550b9fdb74a830525033e0
zyz <zhangyazhou@xylink.com>
2020-01-02 17:00:19 +0800

    添加图片移动

--- UI/ImagePage.xaml.cs
+++ UI/ImagePage.xaml.cs
@@ -18,20 +18,38 @@ namespace WPFDemo.UI
     /// <summary>
     /// Interaction logic for ImagePage.xaml
     /// </summary>
-    public partial class ImagePage : Page
-    {
+    public partial class ImagePage : Page {
+        private bool IsDownInImage;
+        private Point StartPoint;
+
         public ImagePage()
         {
             InitializeComponent();
         }
 
         private void ImageControl_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            IsDownInImage = true;
+            StartPoint = e.GetPosition(PreImage);
         }
 
         private void ImageControl_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            IsDownInImage = false;
         }
 
         private void ImageControl_OnMouseMove(object sender, MouseEventArgs e) {
+            if (IsDownInImage && e.LeftButton == MouseButtonState.Pressed) {
+                Point endPoint = e.GetPosition(PreImage);
+                if (PreImage.RenderTransform is TransformGroup transformGroup) {
+                    if (transformGroup.Children[0] is TranslateTransform translateTransform) {
+                        var x = endPoint.X - StartPoint.X;
+                        var y = endPoint.Y - StartPoint.Y;
+                        translateTransform.X = x;
+                        translateTransform.Y = y;
+                        // StartPoint = endPoint;
+                    }
+
+                }
+            }
         }
 
         private void ImageControl_OnMouseWheel(object sender, MouseWheelEventArgs e) {
