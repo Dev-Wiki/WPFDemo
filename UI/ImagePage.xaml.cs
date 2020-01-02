126d5be6ea726e51ee218e1b3efb930283ac7ca6
zyz <zhangyazhou@xylink.com>
2020-01-02 22:37:41 +0800

    基本完成缩放功能

--- UI/ImagePage.xaml.cs
+++ UI/ImagePage.xaml.cs
@@ -1,35 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
-namespace WPFDemo.UI
-{
+namespace WPFDemo.UI {
     /// <summary>
     /// Interaction logic for ImagePage.xaml
     /// </summary>
     public partial class ImagePage : Page {
         private bool IsDownInImage;
         private Point StartPoint;
+        private double mScaleValue = 1.0;
 
-        public ImagePage()
-        {
+        public ImagePage() {
             InitializeComponent();
         }
 
         private void ImageControl_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
             IsDownInImage = true;
-            StartPoint = e.GetPosition(PreImage);
+            StartPoint = e.GetPosition(sender as UIElement);
         }
 
         private void ImageControl_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
@@ -38,24 +28,59 @@ namespace WPFDemo.UI
 
         private void ImageControl_OnMouseMove(object sender, MouseEventArgs e) {
             if (IsDownInImage && e.LeftButton == MouseButtonState.Pressed) {
-                Point endPoint = e.GetPosition(PreImage);
+                Point endPoint = e.GetPosition(sender as UIElement);
                 if (PreImage.RenderTransform is TransformGroup transformGroup) {
                     if (transformGroup.Children[0] is TranslateTransform translateTransform) {
                         var x = endPoint.X - StartPoint.X;
                         var y = endPoint.Y - StartPoint.Y;
-                        translateTransform.X = x;
-                        translateTransform.Y = y;
-                        // StartPoint = endPoint;
+                        translateTransform.X += x;
+                        translateTransform.Y += y;
+                        StartPoint = endPoint;
                     }
-
                 }
             }
         }
 
         private void ImageControl_OnMouseWheel(object sender, MouseWheelEventArgs e) {
+            if ((mScaleValue <= 1.0 && e.Delta < 0) || mScaleValue >= 2.0 && e.Delta > 0) {
+                return;
+            }
+
+            if (PreImage.RenderTransform is TransformGroup transformGroup) {
+                if (transformGroup.Children[1] is ScaleTransform scaleTransform) {
+                    mScaleValue += e.Delta/1000.0;
+                    if (mScaleValue < 1.0) {
+                        mScaleValue = 1.0;
+                    }
+
+                    if (mScaleValue > 2.0) {
+                        mScaleValue = 2.0;
+                    }
+                    Point point = e.GetPosition(sender as UIElement);
+                    scaleTransform.CenterX = point.X;
+                    scaleTransform.CenterY = point.Y;
+                    scaleTransform.ScaleX = mScaleValue;
+                    scaleTransform.ScaleY = mScaleValue;
+                }
+            }
         }
 
         private void OpenBtn_OnClick(object sender, RoutedEventArgs e) {
+        }
+
+        private void RestBtn_OnClick(object sender, RoutedEventArgs e) {
+            if (PreImage.RenderTransform is TransformGroup transformGroup) {
+                if (transformGroup.Children[0] is TranslateTransform translateTransform) {
+                    translateTransform.X = 0;
+                    translateTransform.Y = 0;
+                    StartPoint = new Point(0, 0);
+                }
+
+                if (transformGroup.Children[1] is ScaleTransform scaleTransform) {
+                    scaleTransform.ScaleX = 1;
+                    scaleTransform.ScaleY = 1;
+                }
+            }
         }
     }
 }
